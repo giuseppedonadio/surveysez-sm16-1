e9af58efc385c009093e1201d9dcd2f640b3ba97
giuseppedonadio <gdonad01@seattlecentral.edu>
2016-08-01 14:51:14 -0700

    Version one of the survey class on survey-view.php
    Only the title tag in <h3> are currently used.

--- surveys/survey-view.php
+++ surveys/survey-view.php
@@ -22,76 +22,52 @@ if(isset($_GET['id']) && (int)$_GET['id'] > 0){#proper data must be on querystri
 	myRedirect(VIRTUAL_PATH . "surveys/index.php");
 }
 
-//sql statement to select individual item
-//$sql = "select * from sm16_IceCream where IceCreamID = " . $myID;
-$sql = "select * from sm16_surveys where SurveyID = " . $myID;
-//---end config area --------------------------------------------------
-
-$foundRecord = FALSE; # Will change to true, if record found!
-
-# connection comes first in mysqli (improved) function
-$result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
-
-if(mysqli_num_rows($result) > 0)
-{#records exist - process
-	   $foundRecord = TRUE;
-	   while ($row = mysqli_fetch_assoc($result))
-	   {
-  			$Title = dbOut($row['Title']);
-        $Description = dbOut($row['Description']);
-        //$Calories = (float)$row['Calories'];
-	   }
+$mySurvey = new Survey($myID);
+if($mySurvey->isValid)
+{//load survey title in title tag
+		$config->titleTag = $mySurvey->Title;
+}else{//no survey in title tag
+		$config->titleTag = 'Sorry, no survey';
 }
+//dumpDie($mySurvey);
 
-@mysqli_free_result($result); # We're done with the data!
-
-if($foundRecord)
-{#only load data if record found
-	$config->titleTag = $Title . " survey made with PHP & love!"; #overwrite PageTitle with Muffin info!
-	#Fills <meta> tags.  Currently we're adding to the existing meta tags in config_inc.php
-	#$config->metaDescription = $MetaDescription . ' Seattle Central\'s ITC280 Class Muffins are made with pure PHP! ' . $config->metaDescription;
-	#$config->metaKeywords = $MetaKeywords . ',Muffins,PHP,Fun,Bran,Regular,Regular Expressions,'. $config->metaKeywords;
-}
-/*
-$config->metaDescription = 'Web Database ITC281 class website.'; #Fills <meta> tags.
-$config->metaKeywords = 'SCCC,Seattle Central,ITC281,database,mysql,php';
-$config->metaRobots = 'no index, no follow';
-$config->loadhead = ''; #load page specific JS
-$config->banner = ''; #goes inside header
-$config->copyright = ''; #goes inside footer
-$config->sidebar1 = ''; #goes inside left side of page
-$config->sidebar2 = ''; #goes inside right side of page
-$config->nav1["page.php"] = "New Page!"; #add a new page to end of nav1 (viewable this page only)!!
-$config->nav1 = array("page.php"=>"New Page!") + $config->nav1; #add a new page to beginning of nav1 (viewable this page only)!!
-*/
 # END CONFIG AREA ----------------------------------------------------------
 
 get_header(); #defaults to theme header or header_inc.php
 ?>
-<h3 align="center">Surveys</h3>
+<h3 align="center"><?=$config->titleTag?></h3>
 
-<p>This page, along with <b>ice-cream-list.php</b>, demonstrate a List/View web application.</p>
-<p>It was built on the mysqli shared web application page, <b>demo_shared.php</b></p>
-<p>This page is to be used only with <b>ice-cream-list.php</b>, and is <b>NOT</b> the entry point of the application, meaning this page gets <b>NO</b> link on your web site.</p>
-<p>Use <b>ice-cream-list.php</b> and <b>ice-cream-view.php</b> as a starting point for building your own List/View web application!</p>
+
 <?php
-if($foundRecord)
-{#records exist - show muffin!
-?>
-	<h3 align="center"><?=$Title;?></h3>
-	<div align="center"><a href="<?=VIRTUAL_PATH;?>surveys/index.php">More Survey?!?</a></div>
-	<table align="center">
-		<tr>
-			<td colspan="2">
-				<blockquote><?=$Description;?></blockquote>
-			</td>
-		</tr>
-	</table>
-<?
-}else{//no such muffin!
-    echo '<div align="center">What! No such Survey? There must be a mistake!!</div>';
-    echo '<div align="center"><a href="' . VIRTUAL_PATH . 'surveys/index.php">Another Survey?</a></div>';
-}
-
 get_footer(); #defaults to theme footer or footer_inc.php
-?>
+
+class Survey
+{
+		public $Title = '';
+		public $Description = '';
+		public $SurveyID = 0;
+		public $isValid = false;
+
+		public function __construct($id)
+		{
+				//forcibly cast to an integer
+				$id = (int)$id;
+				$sql = "select * from sm16_surveys where SurveyID = " . $id;
+				$result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+
+				if(mysqli_num_rows($result) > 0)
+				{#records exist - process
+						 $this->SurveyID = $id;
+					   $this->isValid = true;
+					   while ($row = mysqli_fetch_assoc($result))
+					   {
+					  			$this->Title = dbOut($row['Title']);
+					        $this->Description = dbOut($row['Description']);
+					        //$Calories = (float)$row['Calories'];
+					   }
+				}
+				@mysqli_free_result($result); # We're done with the data!
+
+		}# *** end Survey Constructor ***
+
+}# *** end Survey Class ***
